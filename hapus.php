4627c8a6d9826a88da487cfa7ce4d2764f2441ce
Aulia Agus <auliaagust.siswa@smkn4bdg.sch.id>
2020-02-26 09:27:24 +0700

    Ini file dasar

---
+++ hapus.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+    echo "Ini file yang akan dihapus";
+?>
+<p>1. Ayam Goreng Enak<br/></p>
+<p>2. Ikan Goreng Enak<br/></p>
+<p>3. Rendang Goreng Enak<br/></p>
+</body>
+</html>
+
---
+++ tambah.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+ echo "Ini adalah file tambah.php";
+?>
+<p>
+1. Satu tambah satu = 2<br/>
+2. Dua tambah dua = 4<br/>
+3. Tiga tambah tiga = 6<br/>
+
+
+</body>
+</html>
